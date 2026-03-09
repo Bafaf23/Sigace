861b34c50ffdfed3eaded3128564f34fecce53e4
Bryant Facenda <bryantffacen@gmail.com>
2026-03-09 17:41:04 -0400

    Update Toaster position in RootLayout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
         </Providers>
 
         <Toaster
-          position="top-center"
+          position="top-right"
           reverseOrder={false}
           toastOptions={{
             className:
