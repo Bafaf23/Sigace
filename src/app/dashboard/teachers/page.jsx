d8ba2469ada86d8d2d901df8ac034902036c2918
Bryant Facenda <bryantffacen@gmail.com>
2026-05-12 08:13:50 -0400

    parte 3 de slucionado

--- src/app/dashboard/teachers/page.jsx
+++ src/app/dashboard/teachers/page.jsx
@@ -3,43 +3,24 @@ import AccionesRapidas from "@/components/molecules/AccionesRapidas";
 import HeaderDashbord from "@/components/molecules/HeaderDashbord";
 import Reportes from "@/components/molecules/Reportes";
 import Resumenes from "@/components/molecules/Resumenes";
+import { useAuth } from "@/context/AuthContext";
+import Loading from "@/app/dashboard/loading";
+import AccessDenied from "@/components/molecules/AccessDenied";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 
 export default function TeachersPage() {
+  const { user, loading } = useAuth();
   const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  const user = JSON.parse(localStorage.getItem("user"));
-
-  useEffect(() => {
-    setLoading(false);
-  }, [user]);
-
-  if (loading) {
-    return (
-      <div className="flex min-h-screen items-center justify-center bg-slate-50">
-        <div className="flex flex-col items-center gap-2">
-          <div className="h-10 w-10 animate-spin rounded-full border-4 border-indigo-600 border-t-transparent"></div>
-          <p className="font-medium text-slate-600">Verificando acceso...</p>
-        </div>
-      </div>
-    );
-  }
+  
+  if (loading) return <Loading />;
 
   if (!user || user?.role !== "teacher") {
     router.push("/");
-    return (
-      <div className="flex min-h-screen items-center justify-center">
-        <p className="animate-pulse text-lg font-semibold text-red-600">
-          Redireccionando... No tienes permiso para estar aquí.
-        </p>
-      </div>
-    );
+    return <AccessDenied />;
   }
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="animate-in fade-in duration-500">
       <HeaderDashbord user={user} />
       <Resumenes teachersId={user?.id} />
       {/* movil */}
